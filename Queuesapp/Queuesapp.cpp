4e6a4a16f3561c5f35f9f1a7d7d02417cda373ac
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2024-05-30 11:19:15 +0700

    tampilan apakah antrian kosong atau tidak

--- Queuesapp/Queuesapp.cpp
+++ Queuesapp/Queuesapp.cpp
@@ -60,5 +60,13 @@ public:
 		}
 
 	}
+	void display() {
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+		cout << "\nElement in the queue are ...\n";
 
+	}
 };
